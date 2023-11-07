f09e470a1a3b944ec3ee54b831eb3a4753320147
Jessica Lima de Oliveira <20221170150058@ifrn.local>
2023-11-07 17:01:13 -0300

    varias coisas

---
+++ jogo/Assets/Script/GameManager1.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip moeda, vitoria;
+    public TextMeshProUGUI ContadorDeMoedas, parabains;
+    public int restantes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out source);
+        restantes = FindObjectsOfType<Moeda>().Length;
+        ContadorDeMoedas.text = $"Moedas restantes: {restantes}";       
+    }
+
+    // Update is called once per frame
+    
+    public void TirarMoedas(int Valor)
+    {
+        source.PlayOneShot(moeda);
+        restantes = restantes - Valor;
+        ContadorDeMoedas.text = $"Moedas Restantes: {restantes}";
+        if (restantes <= 0)
+        {
+            parabains.text = "Parabéns";
+            source.Stop();
+            source.PlayOneShot(vitoria);
+            Invoke("ProxFase", 5);
+        }
+    }
+    
+}
---
+++ jogo/Assets/Script/Moeda.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeDeGiro = 50;   
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().TirarMoedas(1);
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidadeDeGiro * Time.deltaTime, Space.World);
+    }
+}
